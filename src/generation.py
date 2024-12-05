cf4263b3e3c081eddaba07b243740f5e2490c6c5
JayOrten <jay.orten@gmail.com>
2024-12-04 19:53:05 -0700

    add per token check generation

--- src/generation.py
+++ src/generation.py
@@ -37,16 +37,66 @@ def generate(
     print('prompt_tokens: ', prompt_tokens)
 
     generate_tokens = model.generate(prompt_tokens.to(device=device),
-                                    max_new_tokens=max_gen_len,
-                                    num_beams=10,
-                                    do_sample=True,
-                                    top_p=top_p,
+                                    max_new_tokens=4,
+                                    do_sample=False,
+                                    top_k=10,
                                     pad_token_id=tokenizer.pad_id,
                                     eos_token_id=tokenizer.eos_id,
-                                    bos_token_id=tokenizer.bos_id,)
+                                    bos_token_id=tokenizer.bos_id,
+                                    output_scores=True,
+                                    return_dict_in_generate=True)
     
-    generate_tokens = generate_tokens.tolist()
-    print('generate_tokens: ', generate_tokens)
+    # Create a list of greedy predictions, starting from the highest probabilities and working down
+    greedy_predictions = [] # list of token ids
+
+    scores_original = []
+    scores_sorted = []
+
+    for score in generate_tokens.scores:
+        # transform tensor to array and sort
+        score = score.cpu().detach().tolist()[0]
+        score = [round(s, 4) for s in score]
+        scores_original.append(score)
+        sorted_score = score.copy()
+        sorted_score.sort()
+        scores_sorted.append(sorted_score)
+
+    print('scores_original: ', scores_original)
+    print('scores_sorted: ', scores_sorted)
+
+    # Just do the top 100 greedy predictions
+    for _ in range(500):
+        # Get index of max values in each scores
+        prediction = []
+        for index, score in enumerate(scores_sorted):
+            max_val = score[-1]
+            max_index = scores_original[index].index(max_val)
+
+            prediction.append(max_index)
+
+        greedy_predictions.append(prediction)
+
+        # Drop the min difference
+        min_difference = float('inf')
+        min_index = None
+
+        for index, score in enumerate(scores_sorted):
+            max_val = score[-1]
+            next_max_val = score[-2]
+            difference = max_val - next_max_val
+
+            if difference < min_difference:
+                min_difference = difference
+                min_index = index
+
+        scores_sorted[min_index].pop(-1)
+
+    # print out shape of scores
+    # print(len(generate_tokens.scores)) # 6
+    # print('generate_tokens.scores.shape[0]: ', generate_tokens.scores[0].shape) # 1, 700
+
+    print('greedy_predictions: ', greedy_predictions)
+    generate_tokens = greedy_predictions[0]
 
     if isinstance(tokenizer, SPTokenizer):
         decoded = tokenizer.decode(generate_tokens)
