896246e47b18f5ed596655a0e98350eeb8b3787d
JayOrten <jay.orten@gmail.com>
2024-12-04 19:53:34 -0700

    remove unnecessary function

--- src/lightning/model.py
+++ src/lightning/model.py
@@ -210,12 +210,3 @@ class Model(LightningModule):
         print("torch.cuda.memory_allocated: %fGB"%(torch.cuda.memory_allocated(0)/1024/1024/1024))
         print("torch.cuda.memory_reserved: %fGB"%(torch.cuda.memory_reserved(0)/1024/1024/1024))
         print("torch.cuda.max_memory_reserved: %fGB"%(torch.cuda.max_memory_reserved(0)/1024/1024/1024))
-
-    def is_illegal(self, board, move):
-        try:
-            if board.parse_san(move) in board.legal_moves:
-                return False
-            else:
-                return True
-        except:
-            return True
